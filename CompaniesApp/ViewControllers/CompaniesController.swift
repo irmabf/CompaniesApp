08c4d1a6da139317f04e6fae6044cced6dd3cd0f
Irma <irblancofdz@gmail.com>
2018-07-09 00:01:27 +0200

    Deleting companies

--- CompaniesApp/ViewControllers/CompaniesController.swift
+++ CompaniesApp/ViewControllers/CompaniesController.swift
@@ -38,57 +38,52 @@ class CompaniesController: UITableViewController, CreateCompanyControllerDelegat
         self.companies = companies
         tableView.reloadData()
       })
-     
-      
     }catch let fetchErr{
       print("Failed to fetch err:", fetchErr)
     }
-    
   }
-
+  //  Overrides
   override func viewDidLoad() {
     super.viewDidLoad()
     
     fetchCompanies()
-    
-//    navigationItem.leftBarButtonItem = UIBarButtonItem(title: "TEST ADD", style: .plain, target: self, action: #selector(addCompany))
-    
     view.backgroundColor = .white
-    
     tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellId)
-    
     setupNavigationItems()
-    
     setupTableViewStyle()
   }
   
-  //  Custom functions
-  
-  @objc func handleAddCompany() {
-    let createCompanyController = CreateCompanyController()
-    let navController = CustomNavigationController(rootViewController: createCompanyController)
+  override func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
     
-    createCompanyController.delegate = self
-    
-    present(navController, animated: true, completion: nil)
+    let deleteAction = UITableViewRowAction(style: .destructive, title: "Delete") { (_, indexPath) in
+      let company = self.companies[indexPath.row]
+      print("Attempting to delete company:", company.name ?? "")
+      
+// 1 -    remove the company from our tableview
+      
+      //1.1- Remove the company from the index
+      self.companies.remove(at: indexPath.row)
+      //1.2- Remove the row from the table
+      self.tableView.deleteRows(at: [indexPath], with: .fade)
+      
+// 2 -     delete the company from Core Data
+      let context = CoreDataManager.shared.persistentContainer.viewContext
+      //2.1 - Delete
+      context.delete(company)
+      //2.2 - Persist the deletion
+      do{
+        try context.save()
+      }catch let saveErr{
+        print("Failed to save deletion with error:", saveErr)
+      }
+      
+    }
+    let editAction = UITableViewRowAction(style: .normal, title: "Edit") { (_, indexPath) in
+      let company = self.companies[indexPath.row]
+      print("Attempting to edit company:", company.name ?? "")
+    }
+     return [deleteAction, editAction]
   }
-  
-  //  Custom functions for styles
-  fileprivate func setupTableViewStyle() {
-    //    tableView.separatorStyle = .none
-    //    This code removes the separator from the part of the tableView without cells
-    tableView.tableFooterView = UIView() // blank UIView
-    tableView.backgroundColor =  .darkBlue
-    tableView.separatorColor = .white
-  }
-  
-  fileprivate func setupNavigationItems() {
-    navigationItem.title = "Companies"
-    
-    navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
-  }
-  
-//  Overrides
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     
@@ -114,6 +109,31 @@ class CompaniesController: UITableViewController, CreateCompanyControllerDelegat
   
   override func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
     return 50
+  }
+  
+  //  Custom functions
+  @objc func handleAddCompany() {
+    let createCompanyController = CreateCompanyController()
+    let navController = CustomNavigationController(rootViewController: createCompanyController)
+    
+    createCompanyController.delegate = self
+    
+    present(navController, animated: true, completion: nil)
+  }
+  
+  //  Custom functions for styles
+  fileprivate func setupTableViewStyle() {
+    //    tableView.separatorStyle = .none
+    //    This code removes the separator from the part of the tableView without cells
+    tableView.tableFooterView = UIView() // blank UIView
+    tableView.backgroundColor =  .darkBlue
+    tableView.separatorColor = .white
+  }
+  
+  fileprivate func setupNavigationItems() {
+    navigationItem.title = "Companies"
+    
+    navigationItem.rightBarButtonItem = UIBarButtonItem(image: #imageLiteral(resourceName: "plus").withRenderingMode(.alwaysOriginal), style: .plain, target: self, action: #selector(handleAddCompany))
   }
   
 }
